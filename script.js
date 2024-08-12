4007ed5afe5b95c55e69d71336798bdaba3102a9
Miguel Barroso <miguelreisbarroso@gmail.com>
2024-08-12 20:14:30 +0100

    3ºday
    Co-authored-by: SantosCandido <SantosCandido@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,20 @@
+const heroDiv = document.getElementById('hero');
+const imagens = ['hero1.jpg', 'hero2.jpg', 'hero3.jpg'];
+let index = 0;
+
+
+function trocarBackground() {
+    heroDiv.classList.remove('fade-in');
+    heroDiv.classList.add('fade-out');
+
+    setTimeout(() => {
+        heroDiv.style.backgroundImage = `url(./assets/images/${imagens[index]})`;
+        index = (index + 1) % imagens.length;
+
+        heroDiv.classList.remove('fade-out');
+        heroDiv.classList.add('fade-in');
+    }, 700); // Tempo da transição (deve ser igual ao tempo definido em CSS)
+}
+
+setInterval(trocarBackground, 8000);
+trocarBackground()
