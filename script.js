a08efb9d6b07c6e872bfa3c7ec064cf77cc17712
Miguel Barroso <miguelreisbarroso@gmail.com>
2024-08-16 16:34:50 +0100

    5ºday
    Co-authored-by: SantosCandido <SantosCandido@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const heroDiv = document.getElementById('hero');
 const imagens = ['hero1.jpg', 'hero2.jpg', 'hero3.jpg'];
 let index = 0;
 
-
+// Função para trocar o background
 function trocarBackground() {
     heroDiv.classList.remove('fade-in');
     heroDiv.classList.add('fade-out');
@@ -16,5 +16,28 @@ function trocarBackground() {
     }, 700); // Tempo da transição (deve ser igual ao tempo definido em CSS)
 }
 
-setInterval(trocarBackground, 8000);
-trocarBackground()
+// Verificar se está na página principal
+if (window.location.pathname === '/') {
+    setInterval(trocarBackground, 8000);
+    trocarBackground();
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Obter o parâmetro da URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const tabId = urlParams.get('tab');
+
+    if (tabId) {
+        // Encontrar o botão de aba que ativa a aba desejada
+        const tabTrigger = document.querySelector(`button#${tabId}`);
+
+        if (tabTrigger) {
+            // Criar uma instância de Tab do Bootstrap e exibir
+            const tab = new bootstrap.Tab(tabTrigger);
+            tab.show();
+        } else {
+            console.error(`Aba com ID '${tabId}' não encontrada.`);
+        }
+    }
+});
