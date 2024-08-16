ee7c1ef5c1e61f6c54a03fee09bba485a947dc1e
SantosCandido <flaitas.candido@hotmail.com>
2024-08-16 21:09:54 +0100

    whatsapp e links funcionais

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const heroDiv = document.getElementById('hero');
 const imagens = ['hero1.jpg', 'hero2.jpg', 'hero3.jpg'];
 let index = 0;
 
-// Função para trocar o background
 function trocarBackground() {
     heroDiv.classList.remove('fade-in');
     heroDiv.classList.add('fade-out');
@@ -13,31 +12,78 @@ function trocarBackground() {
 
         heroDiv.classList.remove('fade-out');
         heroDiv.classList.add('fade-in');
-    }, 700); // Tempo da transição (deve ser igual ao tempo definido em CSS)
+    }, 700); // Tempo da transição (deve ser igual ao tempo definido em CSS!!)
 }
 
-// Verificar se está na página principal
+// Verifica se está na página principal
 if (window.location.pathname === '/') {
     setInterval(trocarBackground, 8000);
     trocarBackground();
 }
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Obter o parâmetro da URL
+document.addEventListener("DOMContentLoaded", function() {
     const urlParams = new URLSearchParams(window.location.search);
-    const tabId = urlParams.get('tab');
+    const tab = urlParams.get('tab');
+    const tabButton = document.querySelector(`#${tab}-tab`);
+    const tabContent = document.querySelector(`#${tab}`);
+    const sideButtonRemove = document.querySelector(`#list-servicos-list`);
+    const sideContentRemove = document.querySelector(`#servicos`);
 
-    if (tabId) {
-        // Encontrar o botão de aba que ativa a aba desejada
-        const tabTrigger = document.querySelector(`button#${tabId}`);
+    if (tab == "servico-rodoviario" || tab == "servico-maritimo" || tab == "servico-aereo" || tab == "servico-concierge" || tab == "servico-golf") {
+        const tabButtonRemove = document.querySelector(`#servico-rodoviario-tab`);
+        const tabContentRemove = document.querySelector(`#servico-rodoviario`);
 
-        if (tabTrigger) {
-            // Criar uma instância de Tab do Bootstrap e exibir
-            const tab = new bootstrap.Tab(tabTrigger);
-            tab.show();
-        } else {
-            console.error(`Aba com ID '${tabId}' não encontrada.`);
-        }
+        tabButtonRemove.classList.remove('show', 'active');
+        tabButtonRemove.setAttribute('aria-selected', 'false');
+        tabContentRemove.classList.remove('show', 'active');
+
+        tabButton.classList.add('show', 'active');
+        tabButton.setAttribute('aria-selected', 'true');
+        tabContent.classList.add('show', 'active');
+    }
+    else if (tab == "transfer-vip"){
+        const sideButton = document.querySelector(`#list-transfers-list`);
+        const sideContent = document.querySelector(`#transfers`);
+
+        sideButtonRemove.classList.remove('show', 'active');
+        sideButtonRemove.setAttribute('aria-selected', 'false');
+        sideContentRemove.classList.remove('show', 'active');
+
+        sideButton.classList.add('show', 'active');
+        sideButton.setAttribute('aria-selected', 'true');
+        sideContent.classList.add('show', 'active');
+
+        tabButton.classList.add('show', 'active');
+        tabButton.setAttribute('aria-selected', 'true');
+        tabContent.classList.add('show', 'active');
+    }
+    else if (tab == "tour-lisboa" || tab == "tour-sintra" || tab == "tour-estoril" || tab == "tour-fatima" || tab == "tour-tomar" || tab == "tour-arrabida" || tab == "tour-evora"){
+        const tabButtonRemove = document.querySelector(`#tour-lisboa-tab`);
+        const tabContentRemove = document.querySelector(`#tour-lisboa`);
+        const sideButton = document.querySelector(`#list-tours-list`);
+        const sideContent = document.querySelector(`#tours`);
+
+        sideButtonRemove.classList.remove('show', 'active');
+        sideButtonRemove.setAttribute('aria-selected', 'false');
+        sideContentRemove.classList.remove('show', 'active');
+
+        sideButton.classList.add('show', 'active');
+        sideButton.setAttribute('aria-selected', 'true');
+        sideContent.classList.add('show', 'active');
+
+        tabButtonRemove.classList.remove('show', 'active');
+        tabButtonRemove.setAttribute('aria-selected', 'false');
+        tabContentRemove.classList.remove('show', 'active');
+
+        tabButton.classList.add('show', 'active');
+        tabButton.setAttribute('aria-selected', 'true');
+        tabContent.classList.add('show', 'active');
+    }
+
+    if (window.location.pathname.endsWith("index.html")) {
+        const newUrl = window.location.pathname.replace("index.html", "");
+    
+        history.replaceState(null, "", newUrl);
     }
 });
